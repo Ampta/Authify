0ce0d9a935e31dba8133b3c280381b6c43a3ecbd
nakanu <ampta77000@gmail.com>
2025-11-04 15:06:50 +0530

    initial setup

---
+++ src/main/java/com/ampta/authify/service/ProfileService.java
@@ -0,0 +1,8 @@
+package com.ampta.authify.service;
+
+import com.ampta.authify.io.ProfileRequest;
+import com.ampta.authify.io.ProfileResponse;
+
+public interface ProfileService {
+    ProfileResponse createProfile(ProfileRequest profileRequest);
+}
---
+++ src/main/java/com/ampta/authify/service/ProfileServiceImpl.java
@@ -0,0 +1,48 @@
+package com.ampta.authify.service;
+
+import com.ampta.authify.entity.UserEntity;
+import com.ampta.authify.io.ProfileRequest;
+import com.ampta.authify.io.ProfileResponse;
+import com.ampta.authify.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileServiceImpl implements ProfileService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public ProfileResponse createProfile(ProfileRequest request) {
+        UserEntity newProfile = convertToUserEntity(request);
+        newProfile = userRepository.save(newProfile);
+        return convertToProfileResponse(newProfile);
+    }
+
+    private ProfileResponse convertToProfileResponse(UserEntity newProfile) {
+        return ProfileResponse.builder()
+                .name(newProfile.getName())
+                .email(newProfile.getEmail())
+                .userId(newProfile.getUserId())
+                .isAccountVerified(newProfile.getIsAccountVerified())
+                .build();
+    }
+
+    private UserEntity convertToUserEntity(ProfileRequest request){
+        return UserEntity.builder()
+                .email(request.getEmail())
+                .userId(UUID.randomUUID().toString())
+                .name(request.getName())
+                .password(request.getPassword())
+                .isAccountVerified(false)
+                .resetOtpExpireAt(0L)
+                .verifyOtp(null)
+                .verifyOtpExpireAt(0L)
+                .resetOtp(null)
+                .build();
+
+    }
+}
